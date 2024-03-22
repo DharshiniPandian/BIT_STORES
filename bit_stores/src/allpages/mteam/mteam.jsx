09fc6bfd5b2277b1a046d474ff1ece36f5e89c91
Harinirpm <harinirangammal4@gmail.com>
2024-03-22 17:32:00 +0530

    created allpages

---
+++ bit_stores/src/allpages/mteam/mteam.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Mteam = () => {
+    return (
+        <div>
+            <h1>Mteam page</h1>
+        </div>
+    );
+};
+
+export default Mteam;
---
+++ bit_stores/src/allpages/user/user.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const User = () => {
+    return (
+        <div className='user'>
+            <h1>About page</h1>
+            <p>This is the user page content.</p>
+        </div>
+    );
+};
+
+export default User;
