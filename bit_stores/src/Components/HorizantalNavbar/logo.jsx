57bbfc9ea0afa8ff640e455f4d4a84019346f31d
Harinirpm <harinirangammal4@gmail.com>
2024-03-22 17:40:57 +0530

    created

--- bit_stores/src/Components/HorizantalNavbar/logo.jsx
+++ bit_stores/src/Components/HorizantalNavbar/logo.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Img from '../../assets/logo.png';
+import Img from '../../assets/logo.jpeg';
 const Logo = () => {
     return (
         <div className="nav-logo">
